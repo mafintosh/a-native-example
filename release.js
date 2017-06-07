5e61faa50fa1b551a75c8ce0d61a9acd9857ed0c
Mathias Buus <mathiasbuus@gmail.com>
2017-06-07 18:27:27 +0200

    more things

--- release.js
+++ release.js
@@ -6,9 +6,12 @@ var path = require('path')
 var tar = require('tar-fs')
 var crypto = require('crypto')
 var request = require('request')
+var zlib = require('zlib')
 
 var WINDOWS = process.platform === 'win32'
 var SERVER = 'nodejs-prebuilds.s3.eu-central-1.amazonaws.com'
+
+if (SERVER.indexOf('://') === -1) SERVER = 'http://' + SERVER // no need for https, content addressed
 
 build(false, function (err, buf, name) {
   if (err) process.exit(1)
@@ -20,8 +23,8 @@ build(false, function (err, buf, name) {
 })
 
 function upload (buf, name) {
-  var server = SERVER.indexOf('://') > -1 ? SERVER : 'http://' + SERVER // no need for https, content addressed
-  request.put(server + '/' + name, {body: buf}, function (err, res) {
+  var url = SERVER + '/' + name
+  request.put(url, {body: buf}, function (err, res) {
     if (err) {
       console.error(err.message)
       process.exit(1)
@@ -30,6 +33,12 @@ function upload (buf, name) {
       console.error('Bad status code:', res.statusCode)
       process.exit(2)
     }
+
+    console.log('###############################################')
+    console.log()
+    console.log('PREBUILD WAS SUCCESFUL: ' + url)
+    console.log()
+    console.log('###############################################')
   })
 }
 
@@ -38,7 +47,9 @@ function build (ia32, cb) {
     if (err) return cb(err)
     if (!version) return cb(null, null)
 
-    spawn(WINDOWS ? 'npm.cmd' : 'npm',  ['run', 'prebuild' + (ia32 ? '-ia32' : '')], {stdio: 'inherit'}).on('exit', function (code) {
+    if (ia32) process.env.ARCH = 'ia32'
+
+    spawn(WINDOWS ? 'npm.cmd' : 'npm',  ['run', 'prebuild'], {stdio: 'inherit'}).on('exit', function (code) {
       if (code) return cb(new Error('Build failed'))
 
       var stream = tar.pack(path.join(__dirname, 'prebuilds'))
@@ -52,12 +63,11 @@ function build (ia32, cb) {
       })
 
       stream.on('end', function () {
-        var buf = Buffer.concat(bufs)
-        var hash = crypto.createHash('sha256').update(buf).digest('hex')
-
-        console.log('PREBUILD WAS SUCCESFUL: ' + hash + '.sha256.tar')
-
-        cb(null, buf, hash + '.sha256.tar')
+        lib.gzip(Buffer.concat(bufs), function (err, buf) {
+          if (err) return cb(err)
+          var hash = crypto.createHash('sha256').update(buf).digest('hex')
+          cb(null, buf, hash + '.sha256.tar.gz')
+        })
       })
     })
   })
